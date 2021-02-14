90436b5ea34eae613ef026d65d61ce563fc59219
Morningred <xma50089@gmail.com>
2021-02-14 07:27:56 -0600

    Added configuration class and get configuration from application from appliation props

---
+++ product-service/src/main/java/com/muhutech/productservice/configuration/Configuration.java
@@ -0,0 +1,20 @@
+package com.muhutech.productservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("product-service")
+public class Configuration {
+	
+	private int prodId;
+
+	public int getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}	
+	
+}
--- product-service/src/main/java/com/muhutech/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/muhutech/productservice/controller/ProductController.java
@@ -1,24 +1,39 @@
 package com.muhutech.productservice.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.muhutech.productservice.bean.Product;
+import com.muhutech.productservice.configuration.Configuration;
 
 
 @RestController
 public class ProductController {
 	
-	@RequestMapping("/product/{prodId}")
-	public Product retrieveProduct(@PathVariable int prodId) {
+	@Autowired
+	private Configuration configuration;
+	
+	@RequestMapping("/product/")
+	public Product retrieveProduct() {
+		
+		int prodId = configuration.getProdId();
 		
 		Product product = new Product(prodId);		
-		
 		
 		return product;		
 		
 	}
+	
+//public Product retrieveProduct(@PathVariable int prodId) {
+//		
+//		Product product = new Product(prodId);		
+//		
+//		
+//		return product;		
+//		
+//	}
 
 }
