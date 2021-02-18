929db497f782edab8937c842aea8b79389cf7d15
Morningred <xma50089@gmail.com>
2021-02-17 23:02:30 -0600

    Ref #3, completed the image store

--- product-service/src/main/java/com/muhutech/productservice/filestore/ImageStore.java
+++ product-service/src/main/java/com/muhutech/productservice/filestore/ImageStore.java
@@ -6,24 +6,33 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 public class ImageStore {
-	
+
 	@Autowired
 	private AmazonS3 s3;
-	
-	public void save (String path, String fileName, InputStream InputStream, Optional<Map<String, String>> optionalMetadata) {
-		
+
+	public void save(String path, String fileName, InputStream InputStream,
+			Optional<Map<String, String>> optionalMetadata) {
+
 		ObjectMetadata metadata = new ObjectMetadata();
-		
-		//optionalMetadata.if
-		
-		s3.putObject(path, fileName, InputStream, metadata);
-		
-		
+
+		optionalMetadata.ifPresent(map -> {
+			if (!map.isEmpty()) {
+				map.forEach(metadata::addUserMetadata);
+
+			}
+		});
+
+		try {
+			s3.putObject(path, fileName, InputStream, metadata);
+		} catch (AmazonServiceException ex) {
+			throw new IllegalStateException("Failed to store image to S3", ex);
+		}
+
 	}
-	
 
 }
